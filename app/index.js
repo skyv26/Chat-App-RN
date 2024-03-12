717df67c518e437a1a95b373bd4e4036ff7bb615
Aakash Verma <dev.aakashv@gmail.com>
2024-03-12 20:09:36 +0530

    `Add:` TailwindCSS classes for designing the application

--- app/index.js
+++ app/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { StyleSheet } from "react-native";
 
 export default function Home() {
     return (
-        <View className="pt-20 bg-purple-400">
+        <View className="pt-20 bg-pink-400">
             <Text className='text-3xl text-center'>
                 Home
             </Text>
