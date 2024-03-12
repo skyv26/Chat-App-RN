b1d062db966d29da8a28e242efce6021bf33eee0
Aakash Verma <dev.aakashv@gmail.com>
2024-03-12 18:02:29 +0530

    Add the `index.js` and layout for the expo-router

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+
+export default function Home() {
+    return (
+        <View className="pt-20 bg-purple-400">
+            <Text className='text-3xl text-center'>
+                Home
+            </Text>
+        </View>
+    );
+};
